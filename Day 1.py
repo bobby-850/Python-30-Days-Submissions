41becf99b6cf82aebed9067e5b6eb74b3c52092c
bobby-850 <voto.cb@gmail.com>
2024-06-12 07:03:40 +0300

    Day 1, Intro and Datatypes

---
+++ Day 1.py
@@ -0,0 +1,21 @@
+#Introduction
+
+
+print (2+3) #addition(+)
+print(3-1) #subtraction(-)
+print (2*3)#multiplication(*)
+print(3/2)#division(/)
+print(3**2)#exponential(**)
+print(3%2)#modulus(%)
+print(3//2)#Floor division operator(//)
+
+
+#Checking Data types
+print(type(10)) #Integers
+print(type(3.14)) #Float
+print(type(1+3j))#COmplex
+print(type("Bobby")) #String
+print(type([1,2,3])) #List
+print(type({'Name':'Bobby'}))#Dictionary
+print(type({9.8,3.14,2.7})) #Set
+print(type((9.8,3.14,2.7))) #Tuples
